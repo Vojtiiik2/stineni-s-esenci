34007d3a2d07bf850defa33a94d7f717751dd7ee
Vojtiiik2 <vojtechvov@gmail.com>
2026-03-01 15:23:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -178,7 +178,7 @@ const Header = ({ t, lang, setLang }) => {
             }}
           >
             <img
-              src="assets/img/logo/logo-symbol.svg"
+              src="assets/img/logo/Logo-symbol.svg"
               alt={`${t.brand2} – logo`}
               className="brand-logo"
               loading="lazy"
